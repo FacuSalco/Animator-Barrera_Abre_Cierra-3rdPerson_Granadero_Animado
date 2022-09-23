8741a9d4392acac26fed2ec42b3d9335a103e101
FacuSalco <facusalco26@gmail.com>
2022-09-23 10:35:38 -0300

    Job Done
    Copie a Jero y me copie re bien. :smile:

---
+++ Assets/Scripts/BarreraABEScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarreraABEScript : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+
+    void OnTriggerEnter() //Sin parametros para que detecte cualquier cosa
+    {
+        anim.SetBool("ObjetoEnRango", true);
+    }
+
+    void OnTriggerExit()
+    {
+        anim.SetBool("ObjetoEnRango", false);
+    }
+}
--- Assets/Scripts/BarreraScript.cs
+++ Assets/Scripts/BarreraScript.cs
@@ -20,13 +20,11 @@ public class BarreraScript : MonoBehaviour
 
     void OnTriggerEnter()
     {
-        Debug.Log("Ingreso detectado");
         anim.SetBool("estaAdentro", true);
     }
 
     void OnTriggerExit()
     {
-        Debug.Log("Salida detectada");
         anim.SetBool("estaAdentro", false);
     }
 }
